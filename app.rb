cf05ce697de1ec0eaa4b8213e24ce5afab43570c
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-27 15:14:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pig = PigLatinizer.new
+    @output = pig.piglatinize(params["user_phrase"])
+    erb :output_view
+  end
+
 end
